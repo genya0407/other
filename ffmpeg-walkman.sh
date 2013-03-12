0c088ca0af777029f49af04bae32e25324f2d850
genya0407 <longinus.eva+git@gmail.com>
2013-03-12 18:43:30 +0900

    ??

--- ffmpeg-walkman.sh
+++ ffmpeg-walkman.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in "$@";do
 	ffmpeg -y -i "${i}" -threads 4 \
-	-vcodec libx264 -b:v 384k -s 480x270 -r 29.97 -profile:v baseline \
+	-vcodec libx264 -b:v 384k -s 480x270 -profile:v baseline \
 	-acodec libfaac -b:a 96k -ar 41000 \
 	"_`basename "${i}" .mp4`.mp4"
 done
